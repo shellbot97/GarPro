66db299521062e7907c359e4fd9fe1f326b41fe8
hitesh <hitesh.ingale@mswipe.com>
2018-12-25 21:30:11 +0530

    SQLite added

---
+++ app/src/main/java/com/example/hitesh/myapplication1/DatabaseHelper.java
@@ -0,0 +1,61 @@
+package com.example.hitesh.myapplication1;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "User.db";
+    public static final String TABLE_NAME = "user_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "DATA";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,Data TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String data) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,data);
+        long result = db.insert(TABLE_NAME,null ,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+
+    public boolean updateData(String id,String data) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,data);
+        db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
+        return true;
+    }
+
+    public Integer deleteData (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?",new String[] {id});
+    }
+}
--- app/src/main/java/com/example/hitesh/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/hitesh/myapplication1/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.hitesh.myapplication1;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -23,23 +27,28 @@ public class MainActivity extends AppCompatActivity {
     EditText user_name;
     EditText address;
     EditText phone_number;
-    Switch report;
+    RadioGroup report_status;
+    DatabaseHelper myDb;
+    LocationManager lm;
+    RadioButton statusradioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myDb = new DatabaseHelper(this);
+
         address = findViewById(R.id.locationET);
         user_name = findViewById(R.id.nameET);
         phone_number = findViewById(R.id.phoneET);
 
+        report_status = findViewById(R.id.report_status);
 
     }
 
-    LocationManager lm;
-
     public void submitData(View view){
+
         JSONObject userdata = new JSONObject();
         try {
             userdata.put("name", user_name.getText().toString());
@@ -53,7 +62,63 @@ public class MainActivity extends AppCompatActivity {
 
         String data_user = userdata.toString();
 
+        boolean isInserted = myDb.insertData(data_user);
+        if(isInserted)
+            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_LONG).show();
 
+        int selectedId = report_status.getCheckedRadioButtonId();
+
+        statusradioButton = (RadioButton) findViewById(selectedId);
+
+        switch (selectedId) {
+            case -1:
+                Toast.makeText(MainActivity.this,"Nothing selected", Toast.LENGTH_SHORT).show();
+                break;
+            case 2131165283:
+                Toast.makeText(MainActivity.this,"hello", Toast.LENGTH_SHORT).show();
+                break;
+            case 2131165284:
+                Toast.makeText(MainActivity.this,"world", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                Toast.makeText(MainActivity.this,"Invalid", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
+    }
+
+    public void viewAllUsers(View view)
+    {
+        Cursor res = myDb.getAllData();
+
+        if(res.getCount() == 0) {
+            // show message
+            showMessage("Error","Nothing found");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            buffer.append("Id :"+ res.getString(0)+"\n");
+            buffer.append("Name :"+ res.getString(1)+"\n");
+        }
+
+        // Show all data
+        showMessage("Data",buffer.toString());
+
+
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 
 
